ed99eb2197a192ad8e25cabdb66faad168da7a68
Freddie Boreham <fredsignupemail@gmail.com>
2021-03-17 08:58:10 +0000

    OOH AHH

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 @echo off
 start /min cmd /k "C:\Users\Fredd\Documents\Computer Science\ngrokstartup.bat"
-if not exist "C:\Computer Science\php-server" mkdir "C:\Computer Science\php-server"
-cd "c:\Computer Science\php-server"
+cd php-server
 php -S localhost:4567
